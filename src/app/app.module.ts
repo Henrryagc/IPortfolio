22c3cf6933eb49f6506c76f20f94931aedb08d05
Henrry Gutierrez <henrryagc12@gmail.com>
2022-01-03 20:25:23 -0500

    Estructura básica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
