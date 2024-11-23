a9ddc9b059c9324477355109f3ec4c3c4a2365af
Henrry Gutierrez <henrryagc12@gmail.com>
2024-11-23 00:02:07 -0500

    remove unnecessary app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { fader } from './route-animation'
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
@@ -9,7 +14,7 @@ import { RouterOutlet } from '@angular/router';
     animations: [
         fader
     ],
-    standalone: false
+    imports: [HeaderComponent, ProfileComponent, MainContentComponent, NavComponent, FooterComponent]
 })
 export class AppComponent {
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { SwiperModule } from 'swiper/angular';
+import { PersonalModule } from './app/main-content/pages/projects/modules/personal/personal.module';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        }), SwiperModule, PersonalModule),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
