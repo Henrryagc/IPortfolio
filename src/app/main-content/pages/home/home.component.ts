2324c54d067428ba077ca18770d314188925bb65
Henrry Gutierrez <henrryagc12@gmail.com>
2024-12-21 22:20:24 -0500

    style: update home component layout with timeline for experience and add skills section

--- src/app/main-content/pages/home/home.component.ts
+++ src/app/main-content/pages/home/home.component.ts
@@ -14,6 +14,45 @@ export class HomeComponent implements OnInit {
 
   items = [1,2,3,4,5,6,4,8,5,4,1,2,4,5];
 
+  events = [
+    {
+      year: '2002',
+      title: 'LinkedIn',
+      description: 'A business- and employment-oriented social networking service.',
+      icon: 'assets/icons/linkedin.png',
+    },
+    {
+      year: '2004',
+      title: 'Facebook',
+      description: 'An online social media and social networking service.',
+      icon: 'assets/icons/facebook.png',
+    },
+    {
+      year: '2005',
+      title: 'YouTube',
+      description: 'First large-scale video sharing website.',
+      icon: 'assets/icons/youtube.png',
+    },
+    {
+      year: '2006',
+      title: 'Twitter',
+      description: 'A service for quick, frequent messages.',
+      icon: 'assets/icons/twitter.png',
+    },
+    {
+      year: '2007',
+      title: 'Tumblr',
+      description: 'Lets you effortlessly share anything.',
+      icon: 'assets/icons/tumblr.png',
+    },
+    {
+      year: '2010',
+      title: 'Instagram',
+      description: 'The home for visual storytelling.',
+      icon: 'assets/icons/instagram.png',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -29,6 +68,9 @@ export class HomeComponent implements OnInit {
     console.log(event);
   }
 
+  calculateDelay(index: number): number {
+    return index * 0.2; // Cada elemento se retrasa 0.2 segundos más que el anterior
+  }
   /*underscore() {
 
     let isVisibleUnderScore = true;
