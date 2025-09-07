e2c5c5b76caab79945691e428514ae4df5c2da45
Henrry Gutierrez <hcondori@bytesw.com.pe>
2025-09-06 22:48:46 -0500

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { fader } from './route-animation'
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ProfileComponent } from './profile/profile.component';
 import { MainContentComponent } from './main-content/main-content.component';
@@ -11,20 +9,10 @@ import { FooterComponent } from './footer/footer.component';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    animations: [
-        fader
-    ],
     imports: [HeaderComponent, ProfileComponent, MainContentComponent, NavComponent, FooterComponent],
 })
 export class AppComponent {
 
-  // * TODO : https://angular.io/guide/standalone-components#lazy-loading-a-standalone-component
-
   title = 'iportfolio';
-
-
-  /*prepareRoute(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
-  }*/
 
 }
