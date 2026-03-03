e07d06d223715abbbf1323258e42f944d9da8b85
Henrry Gutierrez <hcondori@bytesw.com.pe>
2026-03-02 23:14:43 -0500

    feat: update certifications component and project configuration

--- src/app/main-content/pages/certifications/certifications.component.ts
+++ src/app/main-content/pages/certifications/certifications.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { MainContentHeaderComponent } from '../../components/main-content-header/main-content-header.component';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
     selector: 'app-certifications',
     templateUrl: './certifications.component.html',
     styleUrls: ['./certifications.component.scss'],
     imports: [
-      MainContentHeaderComponent,
-      CommonModule
-    ],
+    MainContentHeaderComponent
+],
     standalone: true
 })
 export class CertificationsComponent {
